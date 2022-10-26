aa29e00d61f031174282fbaf0de98b3ddf0f8c7d
Vineetha Aredla <112147217+VineethaAredla@users.noreply.github.com>
2022-10-26 16:31:38 -0500

    bonus is added

---
+++ routes/computation.js
@@ -0,0 +1,23 @@
+var express=require('express');
+var router = express.Router();
+
+router.get('/',function(req,res,next){
+   var number1=Math.floor(Math.random())
+   var number2=Math.floor(Math.random())
+   var number3=Math.floor(Math.random())
+   var hypotfunction=Math.hypot(number1);
+   var ceilfunction=Math.ceil(number2);
+   var clz32function=Math.clz32(number3);
+   res.render('computation',
+   {
+    title:'Vineetha Aredla computation',
+    number1:number1,
+    number2:number2,
+    number3:number3,
+    hypot:hypotfunction,
+    ceil:ceilfunction,
+    clz32:clz32function,
+    });
+});
+
+module.exports=router;
