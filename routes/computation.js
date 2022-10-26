5ee54f065867fd2f69566675097a6bf9573806ab
Vineetha Aredla <112147217+VineethaAredla@users.noreply.github.com>
2022-10-26 17:11:38 -0500

    Bonus is added

--- routes/computation.js
+++ routes/computation.js
@@ -2,9 +2,9 @@ var express=require('express');
 var router = express.Router();
 
 router.get('/',function(req,res,next){
-   var number1=Math.random()
-   var number2=Math.random()
-   var number3=Math.random()
+   var number1=Math.random()+1
+   var number2=Math.random()+2
+   var number3=Math.random()+3
    var hypotfunction=Math.hypot(number1);
    var ceilfunction=Math.ceil(number2);
    var clz32function=Math.clz32(number3);
