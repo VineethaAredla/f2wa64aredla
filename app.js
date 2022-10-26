9f90402e653ec294eedf4760b455b6644627aad0
Vineetha Aredla <112147217+VineethaAredla@users.noreply.github.com>
2022-10-26 16:54:29 -0500

    Bonus is added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
 var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 var computationRouter = require('./routes/computation');
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/mydata', mydataRouter);
 app.use('/users', usersRouter);
 app.use('/computation',computationRouter);
